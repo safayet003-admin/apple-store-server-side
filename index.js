7234c5447d66ec9ea721ee5c57a866bdd6f0f262
Safayet <mrishafayet003@gmail.com>
2022-05-19 16:04:26 +0600

    added more coding

--- index.js
+++ index.js
@@ -14,7 +14,76 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        await client.connect()
+        const appleStoreCollection = client.db('appleStore').collection('serviceProducts')
+        //get all post item and shaow all
+        app.get('/products', async (req, res) => {
+            const cursor = appleStoreCollection.find({});
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        // get specific item 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await appleStoreCollection.findOne(query);
+            res.send(result)
+            console.log("loaded");
+        })
+        app.get('/product', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = appleStoreCollection.find(query);
+                const order = await cursor.toArray();
+                res.send(order)
+            }
+            else {
+                res.status(403).send({ message: "forbiden access" })
+            }
+        })
+        //post products
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const tokenInfo = req.headers.authorization;
 
+            const [email, accesstoken] = tokenInfo.split(" ")
+            const decoded = verfyToken(accesstoken)
+            if (email !== decoded.email) {
+                res.send({ success: "unAuthorized user" })
+
+            }
+            else {
+                const result = await mackbookCollection.insertOne(newProduct)
+                res.send({ success: "product add successfully" })
+
+            }
+        })
+        // Update quantity
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const quantity = req.body.updatequantity;
+            console.log(quantity);
+            console.log(quantity);
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
+            const updatePro = {
+                $set: {
+                    quantity
+                }
+            }
+            const result = await mackbookCollection.updateOne(filter, updatePro, option);
+            res.send(result)
+        })
+        //delete 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await mackbookCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally {
 
