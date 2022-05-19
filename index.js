7b8784e2a6f11b2b41bcb0cf054ecf098047e8bf
Safayet <mrishafayet003@gmail.com>
2022-05-19 15:48:50 +0600

    added more some coding

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 4000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json())
 
-//applestore
-//RkGfDv8lGf3Hgvfp3
+const uri = "mongodb+srv://${process.env.DB_USER}:{process.env.DB_PASS}@cluster0.tbjp1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+/* 
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+ */
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Apples Store')
 })
 
 app.listen(port, () => {
